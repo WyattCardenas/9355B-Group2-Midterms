93c25f3b26a607c9fbb6f462f6b3df7d956e00a3
WyattCardenas <2150907@slu.edu.ph>
2017-03-28 22:37:27 +0800

    html restructure(index, notes, schedule, review) + setup page

--- Web Application/scripts/scripts.js
+++ Web Application/scripts/scripts.js
@@ -1,42 +1,182 @@
-$(window).load(function(){
-	$("[data-toggle]").click(function() {
-		var toggle_el = $(this).data("toggle");
-		$(toggle_el).toggleClass("open-sidebar");
-	});
-	$(".swipe-area").swipe({
-		swipeStatus:function(event, phase, direction, distance, duration, fingers){
-			if (phase=="move" && direction =="right") {
-				$(".container").addClass("open-sidebar");
-				return false;
-			}
-			if (phase=="move" && direction =="left") {
-				$(".container").removeClass("open-sidebar");
-				return false;
-			}
-		}
-	}); 
-});
+var getStarted = document.getElementById("get-started");
+var startEnd = document.getElementById("start-end");
+var subjectAddition = document.getElementById("subject-addition");
+var header = document.getElementById("header");
+getStarted.addEventListener("click",start);
 
-function generateMCQuiz() {
-    document.getElementById("multipleChoiceForm").innerHTML += "<input value='MultipleChoice: '></input>";
+function start(){
+  if(startEnd.className === "hide"){
+    startEnd.className = "questions";
+    getStarted.className = "hide";
+    header.className = "";
+  }
 }
 
-function generateIQuiz() {
-    document.getElementById("identificationForm").innerHTML += "<input value='Identification: '></input>";
+var startItems = document.getElementById("start");
+var endItems = document.getElementById("end");
+var submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click",recordSemester);
+
+//RECORD UNG START AND END NUNG SEM
+//WALA PA LOCAL STORAGE
+function recordSemester(){
+  var startMonth = startItems.querySelector("#month").value;
+  var startDay = startItems.querySelector("#day").value;
+  var endMonth = endItems.querySelector("#month").value;
+  var endDay = endItems.querySelector("#day").value;
+  var valid = checkMonths(startMonth,endMonth);
+  if(startMonth == "" || endMonth == ""){
+    document.getElementById("warning").innerHTML = "Please pick a month."; 
+  }else if(startDay == "" || endDay == ""){
+    document.getElementById("warning").innerHTML = "Please pick a day.";
+  }else if(startMonth == endMonth && startDay == endDay){
+    document.getElementById("warning").innerHTML = "same day nagstart at nag end";
+  }else if(valid === "false"){
+    document.getElementById("warning").innerHTML = "di pa nga ang sstart ung sem ang end na agad";
+  }else if(valid === "same"){
+    document.getElementById("warning").innerHTML = "nice " + (endDay - startDay) +" days lang ung sem";
+  }else{
+    if(subjectAddition.className === "hide"){
+      subjectAddition.className = "";
+      startEnd.className = "hide";
+    }
+  }
 }
 
-function generateMTQuiz() {
-    document.getElementById("matchingTypeForm").innerHTML += "<input value='MatchingType: '></input>";
+function checkMonths(sMonth,eMonth){
+  var x = 0;
+  var y = 0;
+  switch (sMonth) {
+    case "January":
+      x = 1;
+      break;
+    case "February":
+      x = 2;
+      break;
+    case "March":
+      x = 3;
+      break;
+    case "April":
+      x = 4;
+      break;
+    case "May":
+      x = 5;
+      break;
+    case "June":
+      x = 6;
+      break;
+    case "July":
+      x = 7;
+      break;
+    case "August":
+      x = 8;
+      break;
+    case "September":
+      x = 9;
+      break;
+    case "October":
+      x = 10;
+      break;
+    case "November":
+      x = 11;
+      break;
+    case "December":
+      x = 12;
+      break;
+    default:
+      x = 0
+      break;
+  }
+  switch (eMonth) {
+    case "January":
+      y = 1;
+      break;
+    case "February":
+      y = 2;
+      break;
+    case "March":
+      y = 3;
+      break;
+    case "April":
+      y = 4;
+      break;
+    case "May":
+      y = 5;
+      break;
+    case "June":
+      y = 6;
+      break;
+    case "July":
+      y = 7;
+      break;
+    case "August":
+      y = 8;
+      break;
+    case "September":
+      y = 9;
+      break;
+    case "October":
+      y = 10;
+      break;
+    case "November":
+      y = 11;
+      break;
+    case "December":
+      y = 12;
+      break;
+    default:
+      y = 0
+      break;
+  }
+  if(y-x < 0){
+    return "false";
+  }else if(y-x == 0){
+    return "same"
+  }else{
+    return "true";
+  }
 }
 
-function generateFIBQuiz() {
-    document.getElementById("fillInTheBlanksForm").innerHTML += "<input value='FillInTheBlanks: '></input>";
+var addSubject = document.getElementById("add-subject");
+var saveSubjects = document.getElementById("save-subjects");
+var subjectForm = document.getElementById("subject-form");
+var listSubjects = document.getElementById("list-subjects");
+addSubject.addEventListener("click",addSub);
+addSubject.addEventListener("click",recordSub);
+saveSubjects.addEventListener("click",saveSubs);
+
+//TEMPORARY, PANG TEST KO LANG HUEHEUUHE
+function addSub(){
+  var cno = subjectForm.querySelector("#cno").value;
+  var cdesc = subjectForm.querySelector("#cdesc").value;
+  var optionNode = document.createElement("option");
+  var optionText = document.createTextNode(cno+","+cdesc);
+  optionNode.appendChild(optionText);
+  listSubjects.appendChild(optionNode);
 }
 
-function saveQuiz() {
-    document.getElementById("reviewer").submit();
+//ADDS SUBJECT TO DAYS THING
+function recordSub(){
+  var recordedSubjs = document.getElementById("recorded-subjs");
+  var cno = subjectForm.querySelector("#cno").value;
+  var cdesc = subjectForm.querySelector("#cdesc").value;
+  var day = subjectForm.querySelector("#days").value;
+  var writeTo = recordedSubjs.querySelector(`#${day}`);
+  var pNode = document.createElement("p");
+  var pText = document.createTextNode(cno+","+cdesc);
+  pNode.appendChild(pText);
+  if( writeTo.className === "hide"){
+    writeTo.className = "";
+    writeTo.appendChild(pNode);
+  }
 }
 
-function success(){
-	document.getElementById("reminders").submit();
+//SAVE THINGY, DI KO ALAM LOCAL STORAGE
+function saveSubs(){
+  document.getElementById("subjs").innerHTML = "saved";
 }
+
+
+
+//INDEX.HTML
+
