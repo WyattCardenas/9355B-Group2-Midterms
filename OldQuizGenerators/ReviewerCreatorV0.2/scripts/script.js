9e5601c22e8c856d700431a1f887c0fc08f066c1
RVKolodzik <2151299@slu.edu.ph>
2017-04-01 22:54:57 +0800

    final Speech recognition
    Speech recognition is now part of the main html and not embedded in an iframe

--- OldQuizGenerators/ReviewerCreatorV0.2/scripts/script.js
+++ OldQuizGenerators/ReviewerCreatorV0.2/scripts/script.js
@@ -1,17 +1,26 @@
+w = 0;
+x = 0;
+y = 0;
+z = 0;
+
 function generateMCQuiz() {
-    document.getElementById("multipleChoiceForm").innerHTML += "<input value='MultipleChoice: '></input>";
+	w = w + 1;
+    document.getElementById("multipleChoiceForm").innerHTML += `<input id=mC${w} value=MultipleChoice: ></input>`;
 }
 
 function generateIQuiz() {
-    document.getElementById("identificationForm").innerHTML += "<input value='Identification: '></input>";
+	x = x + 1;
+    document.getElementById("identificationForm").innerHTML += `<input id=i${x} value=Identification: ></input>`;
 }
 
 function generateMTQuiz() {
-    document.getElementById("matchingTypeForm").innerHTML += "<input value='MatchingType: '></input>";
+	y = y + 1;
+    document.getElementById("matchingTypeForm").innerHTML += `<input id=mT${y} value=MatchingType: ></input>`;
 }
 
 function generateFIBQuiz() {
-    document.getElementById("fillInTheBlanksForm").innerHTML += "<input value='FillInTheBlanks: '></input>";
+	z = z + 1
+    document.getElementById("fillInTheBlanksForm").innerHTML += `<input id=mC${z} value=FillInTheBlanks: ></input>`;
 }
 
 function saveQuiz() {
