99025d0a63eef87f775541f84604924bb4db5618
Ethnica Carantes <2151084@slu.edu.ph>
2017-04-04 11:43:56 +0800

    updated scripts for notes and minor changes in newnotes.html

--- Web Application/scripts/notes.js
+++ Web Application/scripts/notes.js
@@ -3,14 +3,37 @@ function newNotes(){
 	}
 
 function saveNotes(){
-	var titleForm, noteTitle, textForm, noteArea; 
-	titleForm=document.getElementById("titleForm");
-	noteTitle=titleForm.elements["noteTitle"].value;
+	var noteTitle,noteContent; 
+	noteTitle=document.getElementById("titleForm").value;
 	document.getElementById("titleOutput").innerHTML= noteTitle;
 
-	textForm=document.getElementById("textForm");
-	noteArea=textForm.elements["noteArea"].value;
-	document.getElementById("textOutput").innerHTML= noteArea;
+
+	noteContent=document.getElementById("textForm").value;
+	document.getElementById("textOutput").innerHTML= noteContent;
+
+	
+	var noteObject = {
+		"noteTitle" : noteTitle,
+		"noteContent" : noteContent 
+	}
+
+	var noteArray = [];
+
+	noteArray.push(noteObject); 
+
+	var noteJSON = JSON.stringify(noteArray);
+	localStorage.setItem(`notes`, noteJSON);
+	alert("saved"); 
+
 
 
 }
+
+
+function notes(){
+	var notes = JSON.parse(localStorage.getItem("notes"));
+	var noteTitle = notes[0].noteTitle;
+	document.getElementById("titleOutput").innerHTML = `<button onclick="displayNote(${noteTitle})"> ${noteTitle} </button>`;
+
+}
+
